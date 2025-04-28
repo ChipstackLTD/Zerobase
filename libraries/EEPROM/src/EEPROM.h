5780b0466684b7e497dda4367e1d8f56d6f8d996
thevien2507 <vienbtt@chipstack.vn>
2025-04-28 17:20:01 +0700

    feat: EEPROM Zerobase and Zerobase 2

--- libraries/EEPROM/src/EEPROM.h
+++ libraries/EEPROM/src/EEPROM.h
@@ -1,76 +1,50 @@
 /*
-  EEPROM - Enables reading and writing to non-volatile storage in the processor.
-  
-  Uses the Option bytes area in Flash to emulate EEPROM.
-  On the CH32V003 there are 2+24 bytes available. 
-  The first two bytes are Option bytes data0 and data1.
-  All bytes are copied to a 26-byte long byte array in RAM.
-  After changing a value the commit() method is used to write flash.
-
-  Ported by Maxint R&D to CH32, based on multiple sources.
-  Tested on CH32V003 using Arduino IDE 2.3.2 and OpenWCH core 1.0.4. 
-  Includes code from Option Data example of CH32V003fun by CNLOHR.
-  Arduino original copyright (c) 2006 David A. Mellis.  All right reserved.
-  ESP8266 version copyright (c) 2014 Ivan Grokhotkov. All rights reserved.
-  
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  EEPROM.h - Enhanced EEPROM library for CH32V microcontrollers
+  Supporting both CH32V20x (BOARD_ZEROBASE2) and CH32V003 (BOARD_ZEROBASE)
 */
 
 #ifndef EEPROM_h
 #define EEPROM_h
 #include <Arduino.h>
 
-// Required definitions copied from /system/CH32V00x/SRC/Peripheral/src/ch32v00x_flash.c
-/* Flash Control Register bits */
-//#define CR_PG_Set                  ((uint32_t)0x00000001)
-//#define CR_PG_Reset                ((uint32_t)0xFFFFFFFE)
-//#define CR_PER_Set                 ((uint32_t)0x00000002)
-//#define CR_PER_Reset               ((uint32_t)0xFFFFFFFD)
-//#define CR_MER_Set                 ((uint32_t)0x00000004)
-//#define CR_MER_Reset               ((uint32_t)0xFFFFFFFB)
-#define CR_OPTPG_Set               ((uint32_t)0x00000010)
-#define CR_OPTPG_Reset             ((uint32_t)0xFFFFFFEF)
-#define CR_OPTER_Set               ((uint32_t)0x00000020)
-#define CR_OPTER_Reset             ((uint32_t)0xFFFFFFDF)
-#define CR_STRT_Set                ((uint32_t)0x00000040)
-#define CR_LOCK_Set                ((uint32_t)0x00000080)
-//#define CR_PAGE_PG                 ((uint32_t)0x00010000)
-//#define CR_PAGE_ER                 ((uint32_t)0x00020000)
-//#define CR_BUF_LOAD                ((uint32_t)0x00040000)
-//#define CR_BUF_RST                 ((uint32_t)0x00080000)
+// Board-specific flash memory definitions
+#ifdef BOARD_ZEROBASE2
+  // 256-byte page configuration for BOARD_ZEROBASE2
+  #define EEPROM_FLASH_PAGE_SIZE     256     // 256 bytes per page
+  #define EEPROM_FLASH_BASE_ADDR     ((uint32_t)0x08037F00) // Last 256-byte page in 224KB flash
+  #define EEPROM_SIZE                EEPROM_FLASH_PAGE_SIZE // Full page size for EEPROM emulation
+#elif defined(BOARD_ZEROBASE)
+  // 64-byte page configuration for BOARD_ZEROBASE
+  #define EEPROM_FLASH_PAGE_SIZE     64      // 64 bytes per page
+  #define EEPROM_FLASH_BASE_ADDR     ((uint32_t)0x08003FC0) // Last 64-byte page in 16KB flash
+  #define EEPROM_SIZE                EEPROM_FLASH_PAGE_SIZE // Full page size for EEPROM emulation
+#else
+  // Default configuration (use smallest size to be safe)
+  #define EEPROM_FLASH_PAGE_SIZE     64      // 64 bytes per page
+  #define EEPROM_FLASH_BASE_ADDR     ((uint32_t)0x08003FC0) // Assume last 64-byte page
+  #define EEPROM_SIZE                EEPROM_FLASH_PAGE_SIZE
+#endif
 
-/* FLASH Keys */
-//#define RDP_Key                    ((uint16_t)0x00A5)
-#define FLASH_KEY1                 ((uint32_t)0x45670123)
-#define FLASH_KEY2                 ((uint32_t)0xCDEF89AB)
-
+// Include the appropriate flash header based on the board
+#ifdef BOARD_ZEROBASE
+  #include <ch32v00x_flash.h>
+#else
+  #include <ch32v20x_flash.h>
+#endif
 
 class EEPROMClass {
   public:
     EEPROMClass(void);
     ~EEPROMClass(void);
 
-    void begin(void);
+    void begin(size_t size = EEPROM_SIZE);
 
     uint8_t * getDataPtr();
     uint8_t const * getConstDataPtr() const;
-    uint32_t ReadOptionBytes(void);   // return data0 and data1 option bytes including their inversed values
 
-    uint8_t read( int const idx );
-    void write( int const idx, uint8_t const val);     // requires commit() to make data stick
-    void erase(void);     // requires commit() to make data stick
+    uint8_t read(int const idx);
+    void write(int const idx, uint8_t const val);   // requires commit() to make data stick
+    void erase(void);                               // requires commit() to make data stick
 
     bool commit(void);
     bool end(void);
@@ -94,10 +68,10 @@ class EEPROMClass {
       return t;
     }
 
-    size_t length() {return _size;}
+    size_t length() { return _size; }
 
-    uint8_t& operator[](int const address) {return getDataPtr()[address];}
-    uint8_t const & operator[](int const address) const {return getConstDataPtr()[address];}
+    uint8_t& operator[](int const address) { return getDataPtr()[address]; }
+    uint8_t const & operator[](int const address) const { return getConstDataPtr()[address]; }
 
   protected:
     uint8_t* _data = nullptr;
@@ -109,5 +83,4 @@ class EEPROMClass {
 extern EEPROMClass EEPROM;
 #endif
 
-
-#endif		// #ifndef EEPROM_h
+#endif // #ifndef EEPROM_h
