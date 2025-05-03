26ef94691ca1324e6f9d6f21e32acfaeea11a67e
thevien2507 <vienbtt@chipstack.vn>
2025-05-03 22:33:48 +0700

    feat: change Adafruit_TinyUSB.h to ZBPrint.h

---
+++ libraries/ZBPrint/src/ZBPrint.h
@@ -0,0 +1,9 @@
+#ifndef ZBPRINT_H
+#define ZBPRINT_H
+
+// Include the actual Adafruit TinyUSB library
+#include <Adafruit_TinyUSB.h>
+
+// You could add any additional functions or definitions here if needed
+
+#endif // ZBPRINT_H
